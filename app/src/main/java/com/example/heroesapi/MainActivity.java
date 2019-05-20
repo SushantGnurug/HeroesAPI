ce834f00543cfcf1ae5e9f3f4db19126604f0fee
Softwarica College Lab <softwarica@softwarica.com>
2019-05-20 12:06:42 +0545

    image

--- app/src/main/java/com/example/heroesapi/MainActivity.java
+++ app/src/main/java/com/example/heroesapi/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.heroesapi;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ImageView;
+
+import java.io.File;
 
 import heroesapi.HeroesAPI;
 import model.Heroes;
@@ -19,6 +31,43 @@ import url.Url;
 public class MainActivity extends AppCompatActivity {
     private EditText etName, etDesc;
     private Button btnSave;
+    private ImageView imgProfile;
+    String imagePath;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK ){
+            if (data == null){
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            }
+        }
+        Uri uri = data.getData();
+        imagePath = getRealPathFromUri(uri);
+        previewImage(imagePath);
+    }
+
+    private void previewImage(String imagePath) {
+        File imgFile = new File(imagePath);
+        if (imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            imgProfile.setImageBitmap(myBitmap);
+        }
+    }
+
+    private String getRealPathFromUri(Uri uri){
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, projection, null,null,null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return  result;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +77,10 @@ public class MainActivity extends AppCompatActivity {
         etName = findViewById(R.id.etName);
         etDesc = findViewById(R.id.etDesc);
         btnSave = findViewById(R.id.btnSave);
+        imgProfile = findViewById(R.id.imgProfile);
 
-        btnSave.setOnClickListener(new View.OnClickListener() {
+
+     /*   btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 save();
@@ -69,6 +120,36 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+*/
+
+     btnSave.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             save();
+         }
+
+         private void save() {
+         }
+     });
+
+
+     imgProfile.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             BrowseImage();
+         }
+
+         private void BrowseImage() {
+
+             Intent intent = new Intent(Intent.ACTION_PICK);
+             intent.setType("image/*");
+             startActivityForResult(intent,0);
+         }
+
+
+     });
+
+
 
     }
 }
